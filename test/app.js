31651f08e96eac658b18a9489f3fc537c724fa8c
goateeya <li12074987@gmail.com>
2018-06-13 16:46:16 +0800

    1.init newscanner.js, using es6 class 2.chagne log wording

--- test/app.js
+++ test/app.js
@@ -1,9 +1,15 @@
 var fs = require('fs');
-var scanner = require('../lib/scanner.js');
+// var scanner = require('../lib/scanner.js');
+var Scanner = require('../lib/newscanner.js');
 
 // 1.load file which path set in config.js
 // scanner.run();
  
 // 2.input readerStream to scanner
-var readerStream = fs.createReadStream('./input/testcase.2.html');
-scanner.run(readerStream);
+// var readerStream = fs.createReadStream('./input/testcase.2.html');
+// var data = scanner.run(readerStream);
+// console.log(1 + data);
+
+// 3.use newscanner
+var scanner = new Scanner();
+scanner.run();
